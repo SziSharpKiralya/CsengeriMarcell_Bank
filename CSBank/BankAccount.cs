1d325e366af60bbcfc95e78a0adea86eaac065ec
Petrik Diak <diak@petrik.hu>
2025-03-28 11:00:58 +0100

    BankAccount class kész

---
+++ CSBank/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBank
+{
+	internal abstract class BankAccount : BankingService
+	{
+		double balance;
+
+		public double Balance { get => balance; }
+
+		protected BankAccount(Owner owner) : base(owner)
+		{
+		}
+
+		public void Deposit(double amount)
+		{
+			balance += amount;
+		}
+
+		public abstract bool Withdraw(double amount);
+
+	}
+}
