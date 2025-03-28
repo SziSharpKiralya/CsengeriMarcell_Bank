c936f3483a1be66f7a98328184675c76397083cd
Petrik Diak <diak@petrik.hu>
2025-03-28 10:37:52 +0100

    Owner class kész

---
+++ CSBank/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBank
+{
+	internal sealed class Owner
+	{
+		string ownerName;
+
+		public Owner(string ownerName)
+		{
+			OwnerName = ownerName;
+		}
+
+		public string OwnerName { get => ownerName; set => ownerName = value; }
+	}
+}
